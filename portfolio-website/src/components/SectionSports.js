13bde365f37306d339dff569cf1f7e25e40957bc
hadzical79257 <hadzical79257@th-nuernberg.de>
2025-03-14 23:17:05 +0100

    chore: remove test button

--- portfolio-website/src/components/SectionSports.js
+++ portfolio-website/src/components/SectionSports.js
@@ -8,16 +8,11 @@ const SectionSports = () => {
         <section className="about" id="about">
             <h1>Sportliche Karriere</h1>
             <p>Seit 2017 bin ich Mitglied des Perspektivkaders der Deutschen Taekwondo Union.</p>
-            <button onClick={handleClick}>Button</button>
             <Buttons buttons={[
                 { text: "Dutch Open 2024", href: "assets/cv.pdf", target: "_blank", download: false },
             ]} />
         </section>
     );
 };
-
-function handleClick() {
-    window.open = "/dutch-open-2024";
-}
 
 export default SectionSports;
